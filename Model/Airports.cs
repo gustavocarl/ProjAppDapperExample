80d00d0d46ed5fe1daaede13de1bde39b16ccad1
Gustavo Henrique Carl Dos Santos <gustavo_carl@live.com>
2022-04-06 11:18:21 -0300

    Adicionar arquivos de projeto.

---
+++ Model/Airports.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjAppDapperExample.Model
+{
+    public class Airports
+    {
+        #region Propriedades
+
+        public int Id { get; set; }
+
+        public string City { get; set; }
+
+        public string Country { get; set; }
+
+        public string Code { get; set; }
+
+        public string Continent { get; set; }
+
+        #endregion
+
+        #region Method
+
+        public override string ToString()
+        {
+            return "ID: " + Id +
+                "\nCity: " + City +
+                "\nCounty: " + Country +
+                "\nCode: " + Code +
+                "\nContinent: " + Continent;
+        }
+
+        #endregion
+    }
+}
